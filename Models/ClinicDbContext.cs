76ba3f680657f2afc888fa9c327c67daee3e4cae
Ibbe-1 <229647810+Ibbe-1@users.noreply.github.com>
2026-01-26 11:38:49 +0100

    dbccontext och patient tabell

---
+++ Models/ClinicDbContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicDB.Models;
+
+public partial class ClinicDbContext : DbContext
+{
+    public ClinicDbContext()
+    {
+    }
+
+    public ClinicDbContext(DbContextOptions<ClinicDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Patienter> Patienters { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=ClinicDB;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Patienter>(entity =>
+        {
+            entity.HasKey(e => e.PatientId).HasName("PK__Patiente__970EC346E8198BAB");
+
+            entity.ToTable("Patienter");
+
+            entity.HasIndex(e => e.WaitingNumber, "UQ__Patiente__D021E8C67CFC560D").IsUnique();
+
+            entity.Property(e => e.PatientId).HasColumnName("PatientID");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.PhoneNumber)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Patienter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClinicDB.Models;
+
+public partial class Patienter
+{
+    public int PatientId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public int WaitingNumber { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+}
