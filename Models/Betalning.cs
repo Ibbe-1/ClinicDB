bd13befa333d9ebfb8b30255eaf69e6ab5eff717
yonisb77 <yonis_77@hotmail.com>
2026-01-29 17:29:44 +0100

    la till betalning

---
+++ Models/Betalning.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClinicDB.Models;
+
+public partial class Betalning
+{
+    public int BetalningId { get; set; }
+
+    public int PatientId { get; set; }
+
+    public decimal Belopp { get; set; }
+
+    public DateTime Betalningsdatum { get; set; }
+
+    public string Betalningssatt { get; set; } = null!;
+
+    public string Betalningsstatus { get; set; } = null!;
+}
--- Models/ClinicDbContext.cs
+++ Models/ClinicDbContext.cs
@@ -15,11 +15,7 @@ public partial class ClinicDbContext : DbContext
     {
     }
 
-    public virtual DbSet<Bokningar> Bokningars { get; set; }
-
-    public virtual DbSet<Patienter> Patienters { get; set; }
-
-    public virtual DbSet<Personal> Personals { get; set; }
+    public virtual DbSet<Betalning> Betalnings { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
@@ -27,73 +23,22 @@ public partial class ClinicDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Bokningar>(entity =>
+        modelBuilder.Entity<Betalning>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Bokninga__3214EC0796B8826E");
+            entity.HasKey(e => e.BetalningId).HasName("PK__Betalnin__765FF4493DCD2210");
 
-            entity.ToTable("Bokningar");
+            entity.ToTable("Betalning");
 
-            entity.Property(e => e.Skapad)
+            entity.Property(e => e.BetalningId).HasColumnName("BetalningID");
+            entity.Property(e => e.Belopp).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.Betalningsdatum)
                 .HasDefaultValueSql("(getdate())")
                 .HasColumnType("datetime");
-            entity.Property(e => e.StartTid).HasColumnType("datetime");
-            entity.Property(e => e.Status)
-                .HasMaxLength(20)
-                .HasDefaultValue("Bokad");
-
-            entity.HasOne(d => d.Patient).WithMany(p => p.Bokningars)
-                .HasForeignKey(d => d.PatientId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_Bokningar_Patienter");
-
-            entity.HasOne(d => d.Personal).WithMany(p => p.Bokningars)
-                .HasForeignKey(d => d.PersonalId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_Bokningar_Personal");
-        });
-
-        modelBuilder.Entity<Patienter>(entity =>
-        {
-            entity.HasKey(e => e.PatientId).HasName("PK__Patiente__970EC346BD8F04CB");
-
-            entity.ToTable("Patienter");
-
-            entity.HasIndex(e => e.WaitingNumber, "UQ__Patiente__D021E8C6CB769ADD").IsUnique();
-
+            entity.Property(e => e.Betalningssatt).HasMaxLength(50);
+            entity.Property(e => e.Betalningsstatus)
+                .HasMaxLength(30)
+                .HasDefaultValue("Obetald");
             entity.Property(e => e.PatientId).HasColumnName("PatientID");
-            entity.Property(e => e.CreatedAt)
-                .HasDefaultValueSql("(getdate())")
-                .HasColumnType("datetime");
-            entity.Property(e => e.FirstName)
-                .HasMaxLength(50)
-                .IsUnicode(false);
-            entity.Property(e => e.LastName)
-                .HasMaxLength(50)
-                .IsUnicode(false);
-            entity.Property(e => e.PhoneNumber)
-                .HasMaxLength(20)
-                .IsUnicode(false);
-        });
-
-        modelBuilder.Entity<Personal>(entity =>
-        {
-            entity.HasKey(e => e.PersonalId).HasName("PK__Personal__C16BAC15B4B08766");
-
-            entity.ToTable("Personal");
-
-            entity.Property(e => e.PersonalId).HasColumnName("personal_id");
-            entity.Property(e => e.Namn)
-                .HasMaxLength(100)
-                .IsUnicode(false)
-                .HasColumnName("namn");
-            entity.Property(e => e.Telefonnummer)
-                .HasMaxLength(20)
-                .IsUnicode(false)
-                .HasColumnName("telefonnummer");
-            entity.Property(e => e.Yrke)
-                .HasMaxLength(50)
-                .IsUnicode(false)
-                .HasColumnName("yrke");
         });
 
         OnModelCreatingPartial(modelBuilder);
