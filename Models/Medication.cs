39c65b84fce94b19db96091034dea8cfd76736ee
Sukhman Singh <sukhmans970@gmail.com>
2026-02-01 20:43:53 +0100

    another one

--- Models/Medication.cs
+++
@@ -1,9 +0,0 @@
-namespace ClinicDB.Models;
-
-public class Medication
-{
-    public int MedicationId { get; set; }
-    public string Name { get; set; } = null!;
-    public string? Strength { get; set; }  // ex "500 mg"
-    public DateTime CreatedAt { get; set; }
-}
